11f525e430ceec1a856714a6c666391c6a701f62
Adrian gonzalez <adriamhassekgonza@gmail.com>
2024-06-06 13:50:47 -0500

    test to deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'adminPro';
+  counter = 0;
+
+  add(num: number):void{
+    this.counter += num;
+  }
 }
